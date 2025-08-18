55ce407ca6d6811b55b940ba00b3e4e38587c6ed
INGRNET\akuzansk <adam.kuzanski.1@hexagon.com>
2025-08-18 18:29:47 +0200

    Add export symbols

--- cudaSift.h
+++ cudaSift.h
@@ -3,6 +3,17 @@
 
 #include "cudaImage.h"
 
+// Define export macros for different platforms
+#ifdef _WIN32
+    #ifdef CUDASIFT_BUILD
+        #define CUDASIFT_API __declspec(dllexport)  // Export symbols when building the library
+    #else
+        #define CUDASIFT_API __declspec(dllimport)  // Import symbols when using the library
+    #endif
+#else
+    #define CUDASIFT_API  // On Linux/macOS, this can be empty or use __attribute__((visibility("default")))
+#endif
+
 typedef struct {
   float xpos;
   float ypos;   
@@ -32,14 +43,14 @@ typedef struct {
 #endif
 } SiftData;
 
-void InitCuda(int devNum = 0);
-float *AllocSiftTempMemory(int width, int height, int numOctaves, bool scaleUp = false);
-void FreeSiftTempMemory(float *memoryTmp);
-void ExtractSift(SiftData &siftData, CudaImage &img, int numOctaves, double initBlur, float thresh, float lowestScale = 0.0f, bool scaleUp = false, float *tempMemory = 0);
-void InitSiftData(SiftData &data, int num = 1024, bool host = false, bool dev = true);
-void FreeSiftData(SiftData &data);
-void PrintSiftData(SiftData &data);
-double MatchSiftData(SiftData &data1, SiftData &data2);
-double FindHomography(SiftData &data,  float *homography, int *numMatches, int numLoops = 1000, float minScore = 0.85f, float maxAmbiguity = 0.95f, float thresh = 5.0f);
+CUDASIFT_API void InitCuda(int devNum = 0);
+CUDASIFT_API float *AllocSiftTempMemory(int width, int height, int numOctaves, bool scaleUp = false);
+CUDASIFT_API void FreeSiftTempMemory(float *memoryTmp);
+CUDASIFT_API void ExtractSift(SiftData &siftData, CudaImage &img, int numOctaves, double initBlur, float thresh, float lowestScale = 0.0f, bool scaleUp = false, float *tempMemory = 0);
+CUDASIFT_API void InitSiftData(SiftData &data, int num = 1024, bool host = false, bool dev = true);
+CUDASIFT_API void FreeSiftData(SiftData &data);
+CUDASIFT_API void PrintSiftData(SiftData &data);
+CUDASIFT_API double MatchSiftData(SiftData &data1, SiftData &data2);
+CUDASIFT_API double FindHomography(SiftData &data,  float *homography, int *numMatches, int numLoops = 1000, float minScore = 0.85f, float maxAmbiguity = 0.95f, float thresh = 5.0f);
 
 #endif
